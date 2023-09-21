a1e75e9e871a794e220366da07512b147087e7a2
Mozart Soares <mozartarmstrong@gmail.com>
2023-09-20 21:47:12 -0300

    dev commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const form = document.getElementById('form');
+
+function validacao(valorA,valorB) {
+    return valorA < valorB
+}
+
+
+
+addEventListener('sumbmit',function(e){
+        let formValido
+        e.preventDefault();
+})    
+
+console.log(form);
