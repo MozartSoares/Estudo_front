f7966f57d9f83288a6e674938814b17325a166a6
Mozart Soares <mozartarmstrong@gmail.com>
2023-09-30 01:55:09 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#cpf').mask('000.000.000.00', {
+        placeholder: '___.___.___-__'
+    })
+    $('#cep').mask('00000-000', {
+        placeholder: '_____-___'
+    })
+})
