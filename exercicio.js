8821d9dff014738ae16262f5b1265e9ea8bd8e88
Mozart Soares <mozartarmstrong@gmail.com>
2023-11-20 18:39:46 -0300

    final commit

---
+++ exercicio.js
@@ -0,0 +1,7 @@
+"use strict";
+function multiplicacao(numero, numero2) {
+    return numero * numero2;
+}
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
---
+++ exercicio.ts
@@ -0,0 +1,8 @@
+function multiplicacao(numero: number, numero2: number):number {
+    return numero * numero2
+}
+
+function saudacao(nome: string):string {
+    return `Olá ${nome}`
+}
+
